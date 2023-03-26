864713c35e20fe0bc545c87da603a3ebb439219f
ehmiiz <elarsson17@gmail.com>
2023-03-26 16:29:00 +0200

    initial commit

---
+++ Get-BatteryInfo.ps1
@@ -0,0 +1,53 @@
+function Get-BatteryInfo {
+    <#
+    .SYNOPSIS
+        On a system with the binary `upower`, gets battery data
+    .DESCRIPTION
+        Uses `upower` binary to gather battery data on a laptop, filters it using regex and returns using a PSCustomObject
+    .NOTES
+        Author: Emil Larsson, 2023-03-26
+    .EXAMPLE
+        Get-BatteryInfo
+        Gets status on current battery.
+    #>
+    
+    
+    [CmdletBinding()]
+    param()
+
+    # Verifies required binary
+    $ValidateLinuxBinary = (Get-Command upower)
+    if ([String]::IsNullOrEmpty($ValidateLinuxBinary) ) {
+        throw 'Lacking required binary "upower"'
+    }
+
+    # Run upower, filter data
+    $Battery = upower -e | Where-Object { $_ -match "BAT[0-9]" }
+    $regex = '(?i)\b(model|rechargeable|state|warning-level|energy(-full(-design)?|-rate)?|voltage|charge-cycles|time to empty|percentage|capacity|technology)\b'
+    $FilteredData = upower -i $Battery | Select-String $regex -Raw
+
+    $FilteredData = $FilteredData.Replace('  ', '').Replace(": ", ":")
+
+    # Build Object
+    $Object = [PSCustomObject]@{
+        Model            = $FilteredData[0].Split(":")[1]
+        Rechargeable     = if ($FilteredData[1].Split(":")[1] -eq 'yes' ) { $true } else { false }
+        State            = $FilteredData[2].Split(":")[1]
+        WarningLevel     = $FilteredData[3].Split(":")[1]
+        Energy           = $FilteredData[4].Split(":")[1]
+        EnergyEmpty      = $FilteredData[5].Split(":")[1]
+        EnergyFull       = $FilteredData[6].Split(":")[1]
+        EnergyFullDesign = $FilteredData[7].Split(":")[1]
+        EnergyRate       = $FilteredData[8].Split(":")[1]
+        Voltage          = $FilteredData[9].Split(":")[1]
+        ChargeCycles     = $FilteredData[10].Split(":")[1]
+        TimeToEmpty      = $FilteredData[11].Split(":")[1]
+        Percentage       = $FilteredData[12].Split(":")[1]
+        Capacity         = $FilteredData[13].Split(":")[1]
+        Technology       = $FilteredData[14].Split(":")[1]
+    }
+
+    # Return object
+    return $Object
+
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+# linuxinfo
+
+Contains functions that help with getting valuable information on a linux system
+
+## Completed functions:
+
+- Get-FileSystemHelp (help data on `/`, json format with navigation)
+- Get-BatteryInfo (upower parser)
+- Get-SystemUptime (uptime parser) (Get-Uptime exists in Linux, so this function is a bit redundant but was fun to write.)
+
+
+## Planned functions:
+
+- Get-ComputerInfo (Combines everything)
+- Get-HardWareInfo (parse CPU, RAM, Storage, GPU, PCU, Motherboard)
+- Get-DisplayInfo (lspci parser)
+- Get-NetworkInfo (gets network adapter info)
+- Get-PeripheralInfo (gets usb, bluetooth devices, cameras, microphones)
+- Get-OSInfo (Gets install date, os version, cat /etc/os-release)
