9511efb3030d2331f5834f77cc7685ce4a75ab27
Emil Larsson <emil.larsson@veoneer.com>
2023-11-17 14:54:59 +0100

    testing new publishing script and minor fix

--- publish.ps1
+++ publish.ps1
@@ -2,4 +2,9 @@ $ModulePath = "$PSScriptRoot/src/linuxinfo"
 
 # Publish-Module -Path  -NuGetApiKey 
 
+if (-not (Get-Command Publish-PSResource) ) {
+    Set-PSRepository PSGallery -InstallationPolicy Trusted
+    Install-Module PSResourceGet -Force
+}
+
 Publish-PSResource -Path $ModulePath -ApiKey $Env:APIKEY
